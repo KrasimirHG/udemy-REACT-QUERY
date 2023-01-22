a39940d5075293560d3dd50bd0b6c42b9a14ecd3
Krasio <krasimirhg@gmail.com>
2023-01-22 12:02:18 +0200

    second

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -15,8 +15,11 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data } = useQuery('posts', fetchPosts);
-  if (!data) return <div></div>
+  const { data, isLoading, isError, error } = useQuery('posts', fetchPosts, {
+    staleTime: 2000
+  });
+  if (isLoading) return <h3>Loading...</h3>
+  if (isError) return <><h3>Ooops, Something went wrong</h3><p>{error.toString()}</p></>
 
   return (
     <>
